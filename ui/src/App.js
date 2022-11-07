9bb95ee746a0f65bd65a636fb62687c39e14b5fd
Darius Foodeei <darius.foodei@gmail.com>
2022-11-07 23:41:31 +0100

    add ui params

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,20 +1,33 @@
-import logo from "./logo.svg";
 import "./App.css";
+import SwapForm from "./components/SwapForm.js";
+import MetaMask from "./components/MetaMask.js";
+import EventsFeed from "./components/EventsFeed.js";
+import { MetaMaskProvider } from "./contexts/MetaMask";
 
-function App() {
+const config = {
+    token0Address: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    token1Address: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+    poolAddress: "0x9fe46736679d2d9a65f0992f2272de9f3c7fa6e0",
+    managerAddress: "0xcf7ed3acca5a467e9e704c703e8d87f634fb0fc9",
+    ABIs: {
+        ERC20: require("./abi/ERC20.json"),
+        Pool: require("./abi/Pool.json"),
+        Manager: require("./abi/Manager.json"),
+    },
+};
+
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <MetaMaskProvider>
+            <div className="App flex flex-col justify-between items-center w-full h-full">
+                <MetaMask />
+                <SwapForm config={config} />
+                <footer>
+                    <EventsFeed config={config} />
+                </footer>
+            </div>
+        </MetaMaskProvider>
     );
-}
+};
 
 export default App;
