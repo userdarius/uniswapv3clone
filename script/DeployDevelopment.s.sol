b6161706631b2edaf0b2df3bf9637346cf4384fd
Darius Foodeei <darius.foodei@gmail.com>
2022-11-07 23:26:54 +0100

    ui + added deploy scripts

---
+++ script/DeployDevelopment.s.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.8.14;
+
+import "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "forge-std/Script.sol";
+import "../src/UniswapV3Pool.sol";
+import "../src/UniswapV3Manager.sol";
+import "../test/ERC20Mintable.sol";
+
+contract DeployDevelopment is Script {
+    function run() public {
+        uint256 wethBalance = 1 ether;
+        uint256 usdcBalance = 5042 ether;
+        int24 currentTick = 85176;
+        uint160 currentSqrtP = 5602277097478614198912276234240;
+
+        vm.startBroadcast();
+
+        ERC20Mintable token0 = new ERC20Mintable("Wrapped Ether", "WETH", 18);
+        ERC20Mintable token1 = new ERC20Mintable("USD Coin", "USDC", 18);
+
+        UniswapV3Pool pool = new UniswapV3Pool(address(token0), address(token1), currentSqrtP, currentTick);
+        UniswapV3Manager manager = new UniswapV3Manager();
+
+        token0.mint(msg.sender, wethBalance);
+        token1.mint(msg.sender, usdcBalance);
+
+        console.log("WETH address: ", address(token0));
+        console.log("USDC address: ", address(token1));
+        console.log("Pool address: ", address(pool));
+        console.log("Manager address: ", address(manager));
+
+        vm.stopBroadcast();
+    }
+}
---
+++ ui/src/components/EventsFeed.js
@@ -0,0 +1,113 @@
+import { ethers } from "ethers";
+import { useContext, useEffect, useReducer, useState } from "react";
+import { MetaMaskContext } from "../contexts/MetaMask";
+
+const PoolABI = require("../abi/Pool.json");
+
+const getEvents = (pool) => {
+    return Promise.all([
+        pool.queryFilter("Mint", "earliest", "latest"),
+        pool.queryFilter("Swap", "earliest", "latest"),
+    ]).then(([mints, swaps]) => {
+        return Promise.resolve((mints || []).concat(swaps || []));
+    });
+};
+
+const subscribeToEvents = (pool, callback) => {
+    pool.once("Mint", (a, b, c, d, e, f, g, event) => callback(event));
+    pool.once("Swap", (a, b, c, d, e, f, g, event) => callback(event));
+};
+
+const renderAmount = (amount) => {
+    return ethers.utils.formatUnits(amount);
+};
+
+const renderMint = (args) => {
+    return (
+        <span>
+            <strong>Mint</strong>
+            [range: [{args.tickLower}-{args.tickUpper}], amounts: [{renderAmount(args.amount0)},{" "}
+            {renderAmount(args.amount1)}]]
+        </span>
+    );
+};
+
+const renderSwap = (args) => {
+    return (
+        <span>
+            <strong>Swap</strong>
+            [amount0: {renderAmount(args.amount0)}, amount1: {renderAmount(args.amount1)}]
+        </span>
+    );
+};
+
+const renderEvent = (event, i) => {
+    let content;
+
+    switch (event.event) {
+        case "Mint":
+            content = renderMint(event.args);
+            break;
+
+        case "Swap":
+            content = renderSwap(event.args);
+            break;
+    }
+
+    return <li key={i}>{content}</li>;
+};
+
+const isMintOrSwap = (event) => {
+    return event.event === "Mint" || event.event === "Swap";
+};
+
+const cleanEvents = (events) => {
+    return events
+        .sort((a, b) => b.blockNumber - a.blockNumber)
+        .filter((el, i, arr) => {
+            return i === 0 || el.blockNumber != arr[i - 1].blockNumber || el.logIndex != arr[i - 1].logIndex;
+        });
+};
+
+const eventsReducer = (state, action) => {
+    switch (action.type) {
+        case "add":
+            return cleanEvents([action.value, ...state]);
+
+        case "set":
+            return cleanEvents(action.value);
+    }
+};
+
+const EventsFeed = (props) => {
+    const config = props.config;
+    const metamaskContext = useContext(MetaMaskContext);
+    const [events, setEvents] = useReducer(eventsReducer, []);
+    const [pool, setPool] = useState();
+
+    useEffect(() => {
+        if (metamaskContext.status !== "connected") {
+            return;
+        }
+
+        if (!pool) {
+            const newPool = new ethers.Contract(
+                config.poolAddress,
+                PoolABI,
+                new ethers.providers.Web3Provider(window.ethereum)
+            );
+
+            subscribeToEvents(newPool, (event) => setEvents({ type: "add", value: event }));
+
+            getEvents(newPool).then((events) => {
+                setEvents({ type: "set", value: events });
+            });
+
+            setPool(newPool);
+        }
+    }, [metamaskContext.status, events, pool, config]);
+
+    return <ul className="py-6">{events.filter(isMintOrSwap).map(renderEvent)}</ul>;
+};
+
+export default EventsFeed;
