08d678005abdfd10c84716edd30911fb79a49b3b
Darius Foodeei <darius.foodei@gmail.com>
2022-11-09 16:29:53 +0100

    add callback comment

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -106,6 +106,8 @@ contract UniswapV3Pool {
         uint256 balance1Before;
         if (amount0 > 0) balance0Before = balance0();
         if (amount1 > 0) balance1Before = balance1();
+        // using a callback here, even though not user friendly at all
+        // is essential in order to avoid non-contract addresses to interact with the contract
         IUniswapV3MintCallback(msg.sender).uniswapV3MintCallback(amount0, amount1, data);
         if (amount0 > 0 && balance0Before + amount0 > balance0()) revert InsufficientInputAmount();
         if (amount1 > 0 && balance1Before + amount1 > balance1()) revert InsufficientInputAmount();
