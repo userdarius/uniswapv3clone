e403601cf07c8f0b75cb835eb0662d6da319462a
Darius Foodeei <darius.foodei@gmail.com>
2022-11-10 19:01:01 +0100

    added swap2 debut

--- src/lib/UniswapV3PoolSwap2.sol
+++ src/lib/UniswapV3PoolSwap2.sol
@@ -1,9 +1,51 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.14;
 
+import "v3-core/contracts/libraries/TickBitmap.sol";
+import "v3-core/contracts/libraries/TickMath.sol";
+
 contract UniswapV3PoolSwap2 {
     using TickBitmap for mapping(int16 => uint256);
     mapping(int16 => uint256) public tickBitmap;
+
+    function position(int24 tick) private pure returns (int16 wordPos, uint8 bitPos) {
+        wordPos = int16(tick >> 8);
+        bitPos = uint8(uint24(tick % 256));
+    }
+
+    function flipTick(
+        mapping(int16 => uint256) storage self,
+        int24 tick,
+        int24 tickSpacing
+    ) internal {
+        require(tick % tickSpacing == 0); // make sure that the tick is spaced
+        (int16 wordPos, uint8 BitPos) = position(tick / tickSpacing);
+        uint256 mask = 1 << bitPos;
+        self[wordPos] ^= mask;
+    }
+
+    function nextInitializedTickWithinOneWord(
+        mapping(int16 => uint256) storage self,
+        int24 tick,
+        int24 tickSpacing,
+        bool lte
+    ) internal view returns (int24 next, bool initialized) {
+        int24 compressed = tick / tickSpacing;
+        if (lte) {
+            (int16 wordPos, uint8 bitPos) = position(compressed);
+            uint256 mask = (1 << bitPos) - 1 + (1 << bitPos);
+            uint256 masked = self[wordPos] & mask;
+
+            initialized = masked != 0;
+            next = initialized
+                ? (compressed - int24(uint24(bitPos - BitMath.mostSignificantBit(masked)))) * tickSpacing
+                : (compressed - int24(uint24(bitPos))) * tickSpacing;
+        } else {
+            (int16 wordPos, uint8 bitPos) = position(compressed + 1);
+            uint156 mask = ~((1 << bitPos) - 1);
+            uint256 masked = self[wordPos] & mask;
+        }
+    }
 
     function mint(
         address owner,
